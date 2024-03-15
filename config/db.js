c5230f5535a3f5ddaa5fba2c234937657c6876fc
Mohd Mawan Ahmad <maawan18@gmail.com>
2024-03-16 03:28:59 +0530

    Temp settings for aws

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 module.exports = function(){
     
-    mongoose.connect(process.env.DB_URL)
+    mongoose.connect("mongodb+srv://algomingle:maawanisin11@cluster0.anlxofe.mongodb.net/?retryWrites=true&w=majority")
     .then(console.log("DB connected Successfully :)")).catch((err) => {
         console.log("Error in Connecting DB ", err , process.env.DB_URL );
     })
